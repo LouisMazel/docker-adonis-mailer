9069528a60c1fc89f1a97ac7f0f5b911929a1528
Mazel <mazuel.loic@gmail.com>
2022-12-15 13:30:10 +0100

    refactor: add logs (#1)
    * refactor: add logs
    
    * chore: update pnpm-lock

--- app/Controllers/Http/MailerController.ts
+++ app/Controllers/Http/MailerController.ts
@@ -1,3 +1,4 @@
+import Logger from '@ioc:Adonis/Core/Logger'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import TransactionalEmail from 'App/Mailers/TransactionalEmail'
@@ -7,22 +8,45 @@ import { MailBody } from 'types'
 export default class MailerController {
   public async sendTransactional({ request, response }: HttpContextContract) {
     try {
+      Logger.info('Request to send e-mail received')
+
       await request.validate(SendTransactionalEmailValidator)
 
       const body = request.body() as MailBody
 
-      return new TransactionalEmail(body).send()
+      const response = await new TransactionalEmail(body).send()
+
+      Logger.info('Email sent with success')
+
+      return response
     } catch (error) {
+      const errorMessage = error.message ?? error
+
+      Logger.error(`[MailerController](sendTransactional) ${errorMessage}`)
+
       return response.status(500).json({
         code: 500,
         message: 'Email not sent!',
-        error,
+        error: error,
       })
     }
   }
 
-  public preview({ request }: HttpContextContract) {
-    const body = request.body()
-    return new TransactionalEmail(body.user).preview()
+  public preview({ request, response }: HttpContextContract) {
+    try {
+      Logger.info('Request to preview e-mail received')
+      const body = request.body()
+      return new TransactionalEmail(body.user).preview()
+    } catch (error) {
+      const errorMessage = error.message ?? error
+
+      Logger.error(`[MailerController](sendTransactional) ${errorMessage}`)
+
+      return response.status(500).json({
+        code: 500,
+        message: 'Cannot preview e-mail',
+        error: error,
+      })
+    }
   }
 }
