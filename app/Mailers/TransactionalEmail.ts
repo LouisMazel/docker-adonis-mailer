d33f2a720edd8afa213e6ff7b93a348d6cef4ee0
LouisMazel <mazuel.loic@gmail.com>
2022-12-15 13:24:19 +0100

    refactor: add logs

--- app/Mailers/TransactionalEmail.ts
+++ app/Mailers/TransactionalEmail.ts
@@ -1,3 +1,4 @@
+import Logger from '@ioc:Adonis/Core/Logger'
 import Mail, { BaseMailer, MessageContract } from '@ioc:Adonis/Addons/Mail'
 import Env from '@ioc:Adonis/Core/Env'
 import View from '@ioc:Adonis/Core/View'
@@ -28,28 +29,34 @@ export default class TransactionalEmail extends BaseMailer {
   }
 
   public async prepare(message: MessageContract) {
-    const html = await this.getHtml()
+    try {
+      const html = await this.getHtml()
 
-    const fromEmail = this.options.fromEmail ?? Env.get('SENDER_MAIL')
-    const fromName = this.options.fromName ?? Env.get('SENDER_NAME')
+      const fromEmail = this.options.fromEmail ?? Env.get('SENDER_MAIL')
+      const fromName = this.options.fromName ?? Env.get('SENDER_NAME')
 
-    if (!fromEmail) {
-      throw new Error('No sender e-mail provided')
+      if (!fromEmail) {
+        throw String('Missing fromEmail in body or missing SENDER_MAIL in environment variables')
+      }
+
+      const email = message
+        .from(fromEmail, fromName)
+        .to(this.options.toEmail, this.options.toName)
+        .subject(this.options.subject)
+        .encoding('utf-8')
+
+      const replyToEmail = this.options.replyToEmail ?? Env.get('REPLY_TO_MAIL')
+      const replyToName = this.options.replyToName ?? Env.get('REPLY_TO_NAME')
+
+      if (replyToEmail) {
+        email.replyTo(replyToEmail, replyToName)
+      }
+
+      return email.html(html)
+    } catch (error) {
+      Logger.error(`[TransactionalEmail](prepare) ${error.message ?? error}`)
+
+      throw error
     }
-
-    const email = message
-      .from(fromEmail, fromName)
-      .to(this.options.toEmail, this.options.toName)
-      .subject(this.options.subject)
-      .encoding('utf-8')
-
-    const replyToEmail = this.options.replyToEmail ?? Env.get('REPLY_TO_MAIL')
-    const replyToName = this.options.replyToName ?? Env.get('REPLY_TO_NAME')
-
-    if (replyToEmail) {
-      email.replyTo(replyToEmail, replyToName)
-    }
-
-    return email.html(html)
   }
 }
